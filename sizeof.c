d4bd8cc60983efea087f49038029863d064d1581
Konstantin Dorichev <6565761+kdorichev@users.noreply.github.com>
2023-03-08 15:47:31 +0300

    Size of some types

---
+++ sizeof.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("sizeof  char: %lu\n", sizeof(char));
+	printf("sizeof short: %lu\n", sizeof(short));
+	printf("sizeof   int: %lu\n", sizeof(int));
+	printf("sizeof  long: %lu\n", sizeof(long));
+}
