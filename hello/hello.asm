01a71f5731b1102e441e7e97e66bea7c9c0e92a6
Konstantin Dorichev <6565761+kdorichev@users.noreply.github.com>
2023-03-11 10:30:58 +0300

    Move into folder

---
+++ hello/hello.asm
@@ -0,0 +1,48 @@
+	.text
+	.file	"hello.c"
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	movl	$0, -8(%rbp)
+	movabsq	$.L.str, %rdi
+	movb	$0, %al
+	callq	printf
+	movl	%eax, -8(%rbp)
+	movl	-8(%rbp), %esi
+	movabsq	$.L.str.1, %rdi
+	movb	$0, %al
+	callq	printf
+	xorl	%eax, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	.cfi_def_cfa %rsp, 8
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.L.str,@object                  # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"Hello, world\n"
+	.size	.L.str, 14
+
+	.type	.L.str.1,@object                # @.str.1
+.L.str.1:
+	.asciz	"printed %d chars\n"
+	.size	.L.str.1, 18
+
+	.ident	"Ubuntu clang version 13.0.0-2"
+	.section	".note.GNU-stack","",@progbits
+	.addrsig
+	.addrsig_sym printf
