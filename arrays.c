db5d76e9ee885c07f18f8784effa4d014c2cd602
Konstantin Dorichev <6565761+kdorichev@users.noreply.github.com>
2023-03-11 10:27:05 +0300

    Exploring arrays

---
+++ arrays.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define ARRAY_SIZE 10
+
+int main(void)
+{
+	char carr[ARRAY_SIZE];
+	int	 iarr[ARRAY_SIZE] = {10, 20, -10, -20}; // Initialize some elements; the rest will be 0
+
+	snprintf(carr, sizeof(carr), "0123456789012345");
+	printf("%s\n", carr);
+
+	int num = 54;
+	printf("%6d\n", num);
+
+	// Enumeration constants are also ways to declare constants
+	enum days {MON=1, TUE, WED, THU, FRI, SAT, SUN}; // 
+	printf("Monday is %d, Wednesday is %d\n", MON, WED);
+
+	printf("%s\t%lu\n", carr, sizeof(carr));
+
+	for (int i=0; i<ARRAY_SIZE; i++){
+		printf("%d\t%d\n", i, iarr[i]);
+	}
+
+	// In C99 (and as an optional feature in C11), variable-length arrays (VLAs)
+	// can be declared as well. The size of such an array need not be a compile
+	// time constant:
+	printf("Enter the array size: ");
+	int array_size;
+	fscanf(stdin, "%d", &array_size);
+	int var_length_array[array_size];
+	printf("sizeof array = %zu\n", sizeof var_length_array);
+
+	return 0;
+}
