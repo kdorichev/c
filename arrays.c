9753b7c91805455d719515965cd34c1539b06ce4
Konstantin Dorichev <6565761+kdorichev@users.noreply.github.com>
2024-12-18 19:30:41 +0300

    Added size check

--- arrays.c
+++ arrays.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #define ARRAY_SIZE 10
+#define MAX_ARRAY_SIZE 2048
 
 int main(void)
 {
 	char carr[ARRAY_SIZE];
 	int	 iarr[ARRAY_SIZE] = {10, 20, -10, -20}; // Initialize some elements; the rest will be 0
 
+	// write at most size bytes (including the terminating null byte ('\0')) to str
 	snprintf(carr, sizeof(carr), "0123456789012345");
 	printf("%s\n", carr);
 
 	int num = 54;
-	printf("%6d\n", num);
+	printf("%6d\n", num); // padding from the left
 
 	// Enumeration constants are also ways to declare constants
 	enum days {MON=1, TUE, WED, THU, FRI, SAT, SUN}; // 
@@ -25,11 +27,15 @@ int main(void)
 	// In C99 (and as an optional feature in C11), variable-length arrays (VLAs)
 	// can be declared as well. The size of such an array need not be a compile
 	// time constant:
-	printf("Enter the array size: ");
+	printf("Enter the array size: 0...%d elements\n", MAX_ARRAY_SIZE);
 	int array_size;
-	fscanf(stdin, "%d", &array_size);
+	fscanf(stdin, "%u", &array_size);
+	if (array_size > MAX_ARRAY_SIZE){
+		printf("Entered array size %d > %d", array_size, MAX_ARRAY_SIZE);
+		return 1;
+	}
 	int var_length_array[array_size];
-	printf("sizeof array = %zu\n", sizeof var_length_array);
+	printf("sizeof array = %zu bytes\n", sizeof var_length_array);
 
 	return 0;
 }
